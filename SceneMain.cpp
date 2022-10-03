35db612f67bbb63891d59c066d3fc280152faa0f
fko2247024 <fko2247024@FIC-PCROOM3B-17>
2022-10-03 11:20:05 +0900

    わからん

--- SceneMain.cpp
+++ SceneMain.cpp
@@ -1,10 +1,18 @@
 #include "DxLib.h"
 #include "SceneMain.h"
 
+
+namespace
+{
+	// ショットの間隔
+	constexpr int kShotInterval = 16;
+}
+
 SceneMain::SceneMain()
 {
 	m_hPlayerGraphic = -1;
 	m_hShotGraphic = -1;
+	m_shotInterval = 0;
 }
 SceneMain::~SceneMain()
 {
@@ -19,6 +27,7 @@ void SceneMain::init()
 
 	m_player.setHandle(m_hPlayerGraphic);
 	m_player.init();
+	m_player.setMain(this);
 
 	for (auto& shot : m_shot)
 	{
@@ -42,10 +51,29 @@ void SceneMain::update()
 		shot.update();
 	}
 
+	int shotNum = 0;
+	for (auto& shot : m_shot)
+	{
+		if (shot.isExist()) shotNum++;
+	}
+	DrawFormatString(0, 0, GetColor(255, 255, 255), "弾の数：%d", shotNum);
+
+	m_shotInterval--;
+	if (m_shotInterval < 0) m_shotInterval = 0;
+
 	// キー入力処理
 	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-	if (padState & PAD_INPUT_1)
+	if ((padState & PAD_INPUT_1) && (m_shotInterval <= 0))
 	{
+		for (auto& shot : m_shot)
+		{
+			if (shot.isExist()) continue;
+			
+			shot.start(m_player.getPos());
+			m_shotInterval = kShotInterval;
+			break;
+			
+		}
 	}
 }
 
@@ -58,4 +86,11 @@ void SceneMain::draw()
 	{
 		shot.draw();
 	}
+}
+
+bool SceneMain::createShot(Vec2 pos)
+{
+	if (shot.isExist()) continue;
+
+	shot.start
 }
